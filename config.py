56e9e3e1d0f476141472ac91c93b212b0e493fc9
omk2207 <om@ompro.dev>
2025-05-14 22:16:13 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# Bot configuration
+
+# Bot token from BotFather
+TOKEN = '6972425077:AAG1-KTOtuR-qVO6siEP1sOnyilWbds8Sy4'
+
+# Bot owner details
+OWNER_ID = 6008343239
+OWNER_USERNAME = "@rundilundlegamera"
+
+# MongoDB connection string
+MONGODB_URI = "mongodb://omichewtiya:xZFWzBuCiegH1sfa@cluster0-shard-00-00.f016yeg.mongodb.net:27017,cluster0-shard-00-01.f016yeg.mongodb.net:27017,cluster0-shard-00-02.f016yeg.mongodb.net:27017/?replicaSet=atlas-abcdef-shard-0&ssl=true&authSource=admin"
+
+console.log("Created config.py - Configuration file with bot token and MongoDB URI")
